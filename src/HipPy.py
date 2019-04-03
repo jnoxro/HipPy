7c8995b2a809e5bdac32501663d92ca9a895e034
Jack <jackorton32@gmail.com>
2019-04-03 17:17:46 +0100

    kdhsrjsdjifdug

--- src/HipPy.py
+++ src/HipPy.py
@@ -1,12 +1,41 @@
 """
-sjnfdjhdfihdaigdalifgh
+Hex Image Processing with Python
+
+Authors:
+Ahmed Abbas
+Jack Orton
 """
+systype = 0                         #0 = pi, 1 = win
+width = 640                         #choose raw image width
+height = 480                        #choose raw image height
 
-from methods.getimg import getimg
-import numpy
+
+from methods import *               #import our custom methods
+from picamera.array import PiRGBArray
+from picamera import PiCamera       #import library to interface pi camera
+import cv2                          #import image processing library
+import numpy                        #import numpy
+import time                         #import time(for timing)
 
 
-width = 100
-height = 100
+image = np.zeros(width, height)          # V E C T O R I Z E 
+imageprocessed = np.zeros(170,770)
 
-getimg()
+#setup
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+while true:
+    if systype == 0:                           #if on pi
+        image = getimg(camera, rawCapture)     #Capture image
+    if systype == 1:                           #if on lapop
+        image = getimgwin()                    #capture image
+    
+    imageprocessed, tar, contour = procimg(image)   #process the image, Tar = True/False (target found?), contour(target outline)
+
+    if tar:
+        
+
+
+
+
