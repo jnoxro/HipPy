2e3faab71bea8c7f9e28660890f4b17c55c7772b
Jack <jackorton32@gmail.com>
2019-05-01 13:13:26 +0100

    v4l2

--- src/v4l2test.py
+++ src/v4l2test.py
@@ -2,6 +2,10 @@
 (v4l2 needs python 2)
 do this thing: sudo modprobe v4l2loopback
 
+update (may do python 3 yay)
+https://github.com/jremmons/pyfakewebcam/blob/master/examples/flashing_doge.py
+modprobe v4l2loopback devices=2
+
 """
 import fcntl, sys, os
 print("imported fcntl, sys and os")
